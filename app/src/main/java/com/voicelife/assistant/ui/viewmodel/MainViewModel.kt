f38d98d0dbb00d867b61235dfe1caaa59d3fa5ed
yangyayuan <741103702@qq.com>
2025-12-06 11:30:18 +0800

    Fix missing File import in MainViewModel

--- app/src/main/java/com/voicelife/assistant/ui/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/voicelife/assistant/ui/viewmodel/MainViewModel.kt
@@ -15,6 +15,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import java.io.File
 import javax.inject.Inject
 
 /**
